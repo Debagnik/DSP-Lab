8c978c5af9e1f0d71f8f958abf551b4cc85458f0
Debagnik <debagnikkar.3@gmail.com>
2021-02-03 14:32:21 +0530

    Fix 3

--- lab20012021/idft0.m
+++ lab20012021/idft0.m
@@ -3,7 +3,7 @@
 function Xk = idft0(xn,N)
     for k = 0:N-1
         for n = 0:N-1
-            Wn = exp(1j*2*pi*k'*n/N)
+            Wn(k+1,n+1) = exp(1j*2*pi*k'*n/N)
             
         end
     end
--- lab20012021/main_2.m
+++ lab20012021/main_2.m
@@ -4,12 +4,12 @@ clc
 close all
 clear all
 
-x = input('Enter the 1st sequence:')
-h = input('Enter the 2nd sequence:')
+x = input('Enter the 1st sequence: ')
+h = input('Enter the 2nd sequence: ')
 n0 = 0:(length(x)+length(h)-2)
 n1 = 0:max(length(x),length(h))-1
-lc = lenearconv(x,h)
-cc = circonv(x,h)
+lc = lenearconv0(x,h)
+cc = circonv0(x,h)
 subplot 411
 stem(x,'c')
 title('Input Sequence 1')
